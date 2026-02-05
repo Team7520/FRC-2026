2ad8a591929c87e1dae3c97d119fea318d5ad803
al-1108 <altyw69420@gmail.com>
2026-02-04 20:09:33 -0500

    Created turret subsystem with placeholder methods
    - Created turret subsystem
    - Created methods to run each motor

---
+++ src/main/java/frc/robot/subsystems/TurretSubsystem.java
@@ -0,0 +1,54 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.hardware.TalonFX;
+
+//for current limit if we add
+// import com.ctre.phoenix6.configs.TalonFXConfiguration;
+// import com.ctre.phoenix6.controls.DutyCycleOut;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix6.controls.DutyCycleOut;
+
+
+public class TurretSubsystem extends SubsystemBase{
+    private final TalonFX turnMotor;
+    private final TalonFX hoodMotor;
+    private final TalonFX topMotor;
+    private final TalonFX bottomMotor;
+    private final DutyCycleOut duty = new DutyCycleOut(0);
+
+    public TurretSubsystem(int turnMotorId, int hoodMotorId, int topMotorId, int bottomMotorId) {
+        turnMotor = new TalonFX(turnMotorId);
+        hoodMotor = new TalonFX(hoodMotorId);
+        topMotor = new TalonFX(topMotorId);
+        bottomMotor = new TalonFX(bottomMotorId);
+
+        //possibly add current limits
+        turnMotor.setNeutralMode(com.ctre.phoenix6.signals.NeutralModeValue.Brake);
+        hoodMotor.setNeutralMode(com.ctre.phoenix6.signals.NeutralModeValue.Brake);
+        topMotor.setNeutralMode(com.ctre.phoenix6.signals.NeutralModeValue.Brake);
+        bottomMotor.setNeutralMode(com.ctre.phoenix6.signals.NeutralModeValue.Brake);
+    }
+
+    public void turn(double speed) {
+        turnMotor.setControl(duty.withOutput(speed));
+    }
+
+    public void hood(double speed) {
+        hoodMotor.setControl(duty.withOutput(speed));
+    }
+
+    public void top(double speed) {
+        topMotor.setControl(duty.withOutput(speed));
+    }
+
+    public void bottom(double speed) {
+        bottomMotor.setControl(duty.withOutput(speed));
+    }
+
+    public void stopAll() {
+        turnMotor.setControl(duty.withOutput(0));
+        hoodMotor.setControl(duty.withOutput(0));
+        topMotor.setControl(duty.withOutput(0));
+        bottomMotor.setControl(duty.withOutput(0));
+    }
+}
