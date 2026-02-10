ec8f4ecaa2d6311bff3dfc4cf29dad604e0cad55
Another-Person1 <woodyl_2009@hotmail.com>
2026-02-10 11:25:31 -0500

    made a separate branch to test yams out, asked codex to fix code and update things to yams

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -1,57 +1,100 @@
 package frc.robot.subsystems;
 
-import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.controls.DutyCycleOut;
-import com.ctre.phoenix6.controls.PositionDutyCycle;
+import static edu.wpi.first.units.Units.*;
+
 import com.ctre.phoenix6.hardware.TalonFX;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.Time;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.generated.TunerConstants;
+import yams.gearing.GearBox;
+import yams.gearing.MechanismGearing;
+import yams.motorcontrollers.SmartMotorControllerConfig;
+import yams.motorcontrollers.SmartMotorControllerConfig.ControlMode;
+import yams.motorcontrollers.SmartMotorControllerConfig.MotorMode;
+import yams.motorcontrollers.remote.TalonFXWrapper;
 
 public class IntakeSubsystem extends SubsystemBase {
 
-  private final TalonFX intakeMotor;
-  private final TalonFX extendMotor;
-  private final TalonFX indexerMotor;
-  private final DutyCycleOut duty = new DutyCycleOut(0);
-  private final PositionDutyCycle pivotPosReq = new PositionDutyCycle(0);
-  double extendedPosition;
-  double retractedPosition;
+  private static final int INTAKE_MOTOR_ID = 1;
+  private static final int EXTEND_MOTOR_ID = 2; // placeholder ids
+  private static final int INDEXER_MOTOR_ID = 3;
+
+  private static final Time OPEN_LOOP_RAMP = Seconds.of(0.15);
+  private static final Angle EXTENDED_POSITION = Degrees.of(90);
+  private static final Angle RETRACTED_POSITION = Degrees.of(0);
+
+  private final TalonFXWrapper intakeMotor;
+  private final TalonFXWrapper extendMotor;
+  private final TalonFXWrapper indexerMotor;
+
+  private Angle extendedPosition = EXTENDED_POSITION;
+  private Angle retractedPosition = RETRACTED_POSITION;
 
   public IntakeSubsystem() {
-    intakeMotor = new TalonFX(1);
-    extendMotor = new TalonFX(2); // placeholder ids
-    indexerMotor = new TalonFX(3);
+    SmartMotorControllerConfig intakeConfig =
+        new SmartMotorControllerConfig(this)
+            .withStatorCurrentLimit(Amps.of(40))
+            .withOpenLoopRampRate(OPEN_LOOP_RAMP)
+            .withIdleMode(MotorMode.COAST)
+            .withControlMode(ControlMode.OPEN_LOOP);
 
-    TalonFXConfiguration config = new TalonFXConfiguration();
-    config.CurrentLimits.StatorCurrentLimitEnable = true;
-    config.CurrentLimits.StatorCurrentLimit = 40.0;
+    SmartMotorControllerConfig extendConfig =
+        new SmartMotorControllerConfig(this)
+            .withClosedLoopController(
+                4.0, 0.0, 0.0, DegreesPerSecond.of(180), DegreesPerSecondPerSecond.of(90))
+            .withSoftLimit(Degrees.of(-30), Degrees.of(100))
+            .withGearing(new MechanismGearing(GearBox.fromReductionStages(3, 4)))
+            .withIdleMode(MotorMode.BRAKE)
+            .withStatorCurrentLimit(Amps.of(40))
+            .withControlMode(ControlMode.CLOSED_LOOP)
+            .withStartingPosition(retractedPosition);
 
-    intakeMotor.getConfigurator().apply(config);
-    intakeMotor.setNeutralMode(com.ctre.phoenix6.signals.NeutralModeValue.Brake);
-    extendMotor.getConfigurator().apply(config);
-    extendMotor.setNeutralMode(com.ctre.phoenix6.signals.NeutralModeValue.Brake);
-    indexerMotor.getConfigurator().apply(config);
-    indexerMotor.setNeutralMode(com.ctre.phoenix6.signals.NeutralModeValue.Brake);
+    intakeMotor =
+        new TalonFXWrapper(
+            new TalonFX(INTAKE_MOTOR_ID, TunerConstants.kCANBus),
+            DCMotor.getKrakenX60(1),
+            intakeConfig);
+    extendMotor =
+        new TalonFXWrapper(
+            new TalonFX(EXTEND_MOTOR_ID, TunerConstants.kCANBus),
+            DCMotor.getKrakenX60(1),
+            extendConfig);
+    indexerMotor =
+        new TalonFXWrapper(
+            new TalonFX(INDEXER_MOTOR_ID, TunerConstants.kCANBus),
+            DCMotor.getKrakenX60(1),
+            intakeConfig);
   }
 
   public void runIntake(double speed) {
-    intakeMotor.setControl(duty.withOutput(speed));
+    intakeMotor.setDutyCycle(speed);
   }
 
   public void extend() {
-    extendMotor.setControl(pivotPosReq.withPosition(extendedPosition));
+    extendMotor.setPosition(extendedPosition);
   }
 
   public void retract() {
-    extendMotor.setControl(pivotPosReq.withPosition(retractedPosition));
+    extendMotor.setPosition(retractedPosition);
   }
 
   public void runIndexer(double speed) {
-    indexerMotor.setControl(duty.withOutput(speed));
+    indexerMotor.setDutyCycle(speed);
+  }
+
+  public void setExtendedPosition(Angle position) {
+    extendedPosition = position;
+  }
+
+  public void setRetractedPosition(Angle position) {
+    retractedPosition = position;
   }
 
   public void stopAll() {
-    intakeMotor.setControl(duty.withOutput(0));
-    extendMotor.setControl(duty.withOutput(0));
-    indexerMotor.setControl(duty.withOutput(0));
+    intakeMotor.setDutyCycle(0.0);
+    extendMotor.setDutyCycle(0.0);
+    indexerMotor.setDutyCycle(0.0);
   }
 }
--- src/main/java/frc/robot/subsystems/TurretSubsystem.java
+++ src/main/java/frc/robot/subsystems/TurretSubsystem.java
@@ -1,49 +1,134 @@
 package frc.robot.subsystems;
 
-import com.ctre.phoenix6.controls.DutyCycleOut;
+import static edu.wpi.first.units.Units.*;
+
 import com.ctre.phoenix6.hardware.TalonFX;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.Time;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.generated.TunerConstants;
+import yams.gearing.GearBox;
+import yams.gearing.MechanismGearing;
+import yams.mechanisms.config.PivotConfig;
+import yams.mechanisms.positional.Pivot;
+import yams.motorcontrollers.SmartMotorControllerConfig;
+import yams.motorcontrollers.SmartMotorControllerConfig.ControlMode;
+import yams.motorcontrollers.SmartMotorControllerConfig.MotorMode;
+import yams.motorcontrollers.remote.TalonFXWrapper;
 
 public class TurretSubsystem extends SubsystemBase {
-  private final TalonFX turnMotor;
-  private final TalonFX hoodMotor;
-  private final TalonFX topMotor;
-  private final TalonFX bottomMotor;
-  private final DutyCycleOut duty = new DutyCycleOut(0);
+  private static final Time OPEN_LOOP_RAMP = Seconds.of(0.15);
+
+  private static final Angle TURRET_MIN_ANGLE = Degrees.of(-180);
+  private static final Angle TURRET_MAX_ANGLE = Degrees.of(180);
+  private static final Angle HOOD_MIN_ANGLE = Degrees.of(-10);
+  private static final Angle HOOD_MAX_ANGLE = Degrees.of(45);
+
+  private final TalonFXWrapper turretMotor;
+  private final Pivot turret;
+  private final TalonFXWrapper hoodMotor;
+  private final Pivot hood;
+  private final TalonFXWrapper topMotor;
+  private final TalonFXWrapper bottomMotor;
 
   public TurretSubsystem(int turnMotorId, int hoodMotorId, int topMotorId, int bottomMotorId) {
-    turnMotor = new TalonFX(turnMotorId);
-    hoodMotor = new TalonFX(hoodMotorId);
-    topMotor = new TalonFX(topMotorId);
-    bottomMotor = new TalonFX(bottomMotorId);
+    SmartMotorControllerConfig turretConfig =
+        new SmartMotorControllerConfig(this)
+            .withClosedLoopController(
+                4.0, 0.0, 0.0, DegreesPerSecond.of(180), DegreesPerSecondPerSecond.of(90))
+            .withSoftLimit(TURRET_MIN_ANGLE, TURRET_MAX_ANGLE)
+            .withGearing(new MechanismGearing(GearBox.fromReductionStages(3, 4)))
+            .withIdleMode(MotorMode.BRAKE)
+            .withStatorCurrentLimit(Amps.of(40))
+            .withControlMode(ControlMode.CLOSED_LOOP)
+            .withStartingPosition(Degrees.of(0));
 
-    // possibly add current limits
-    turnMotor.setNeutralMode(com.ctre.phoenix6.signals.NeutralModeValue.Brake);
-    hoodMotor.setNeutralMode(com.ctre.phoenix6.signals.NeutralModeValue.Brake);
-    topMotor.setNeutralMode(com.ctre.phoenix6.signals.NeutralModeValue.Brake);
-    bottomMotor.setNeutralMode(com.ctre.phoenix6.signals.NeutralModeValue.Brake);
+    SmartMotorControllerConfig hoodConfig =
+        new SmartMotorControllerConfig(this)
+            .withClosedLoopController(
+                4.0, 0.0, 0.0, DegreesPerSecond.of(120), DegreesPerSecondPerSecond.of(60))
+            .withSoftLimit(HOOD_MIN_ANGLE, HOOD_MAX_ANGLE)
+            .withGearing(new MechanismGearing(GearBox.fromReductionStages(3, 4)))
+            .withIdleMode(MotorMode.BRAKE)
+            .withStatorCurrentLimit(Amps.of(30))
+            .withControlMode(ControlMode.CLOSED_LOOP)
+            .withStartingPosition(Degrees.of(0));
+
+    SmartMotorControllerConfig shooterConfig =
+        new SmartMotorControllerConfig(this)
+            .withStatorCurrentLimit(Amps.of(40))
+            .withOpenLoopRampRate(OPEN_LOOP_RAMP)
+            .withIdleMode(MotorMode.COAST)
+            .withControlMode(ControlMode.OPEN_LOOP);
+
+    turretMotor =
+        new TalonFXWrapper(
+            new TalonFX(turnMotorId, TunerConstants.kCANBus),
+            DCMotor.getKrakenX60(1),
+            turretConfig);
+    hoodMotor =
+        new TalonFXWrapper(
+            new TalonFX(hoodMotorId, TunerConstants.kCANBus), DCMotor.getKrakenX60(1), hoodConfig);
+    topMotor =
+        new TalonFXWrapper(
+            new TalonFX(topMotorId, TunerConstants.kCANBus),
+            DCMotor.getKrakenX60(1),
+            shooterConfig);
+    bottomMotor =
+        new TalonFXWrapper(
+            new TalonFX(bottomMotorId, TunerConstants.kCANBus),
+            DCMotor.getKrakenX60(1),
+            shooterConfig);
+
+    turret =
+        new Pivot(
+            new PivotConfig(turretMotor)
+                .withHardLimit(TURRET_MIN_ANGLE, TURRET_MAX_ANGLE)
+                .withStartingPosition(Degrees.of(0)));
+    hood =
+        new Pivot(
+            new PivotConfig(hoodMotor)
+                .withHardLimit(HOOD_MIN_ANGLE, HOOD_MAX_ANGLE)
+                .withStartingPosition(Degrees.of(0)));
+  }
+
+  public void setTurretAngle(Angle angle) {
+    turretMotor.setPosition(angle);
+  }
+
+  public void setHoodAngle(Angle angle) {
+    hoodMotor.setPosition(angle);
+  }
+
+  public Angle getTurretAngle() {
+    return turret.getAngle();
+  }
+
+  public Angle getHoodAngle() {
+    return hood.getAngle();
   }
 
   public void turn(double speed) {
-    turnMotor.setControl(duty.withOutput(speed));
+    turretMotor.setDutyCycle(speed);
   }
 
   public void hood(double speed) {
-    hoodMotor.setControl(duty.withOutput(speed));
+    hoodMotor.setDutyCycle(speed);
   }
 
   public void top(double speed) {
-    topMotor.setControl(duty.withOutput(speed));
+    topMotor.setDutyCycle(speed);
   }
 
   public void bottom(double speed) {
-    bottomMotor.setControl(duty.withOutput(speed));
+    bottomMotor.setDutyCycle(speed);
   }
 
   public void stopAll() {
-    turnMotor.setControl(duty.withOutput(0));
-    hoodMotor.setControl(duty.withOutput(0));
-    topMotor.setControl(duty.withOutput(0));
-    bottomMotor.setControl(duty.withOutput(0));
+    turretMotor.setDutyCycle(0.0);
+    hoodMotor.setDutyCycle(0.0);
+    topMotor.setDutyCycle(0.0);
+    bottomMotor.setDutyCycle(0.0);
   }
 }
