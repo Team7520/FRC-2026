7c58f446487e8b8bc2658743d297d7df7a423012
NathanS32 <nathan.song@team7520.com>
2026-02-19 20:47:57 -0500

    added commands to run intake

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -4,6 +4,10 @@ import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.DutyCycleOut;
 import com.ctre.phoenix6.controls.PositionDutyCycle;
 import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class IntakeSubsystem extends SubsystemBase {
@@ -12,19 +16,24 @@ public class IntakeSubsystem extends SubsystemBase {
   private final TalonFX extendMotor;
   private final DutyCycleOut duty = new DutyCycleOut(0);
   private final PositionDutyCycle pivotPosReq = new PositionDutyCycle(0);
-  double extendedPosition;
-  double retractedPosition;
+  double extendedPosition = 0;
+  double retractedPosition = -16.4794921875;
 
   public IntakeSubsystem() {
-    intakeMotor = new TalonFX(1);
-    extendMotor = new TalonFX(2); // placeholder ids
+    intakeMotor = new TalonFX(57);
+    extendMotor = new TalonFX(58);
 
     TalonFXConfiguration config = new TalonFXConfiguration();
+    config.Slot0.kP = 2;
+    config.Slot0.kI = 0;
+    config.Slot0.kD = 0;
     config.CurrentLimits.StatorCurrentLimitEnable = true;
     config.CurrentLimits.StatorCurrentLimit = 40.0;
 
     intakeMotor.getConfigurator().apply(config);
     intakeMotor.setNeutralMode(com.ctre.phoenix6.signals.NeutralModeValue.Brake);
+    config.CurrentLimits.SupplyCurrentLimitEnable = true;
+    config.CurrentLimits.SupplyCurrentLimit = 10;
     extendMotor.getConfigurator().apply(config);
     extendMotor.setNeutralMode(com.ctre.phoenix6.signals.NeutralModeValue.Brake);
   }
@@ -48,5 +57,27 @@ public class IntakeSubsystem extends SubsystemBase {
   public void stopAll() {
     intakeMotor.setControl(duty.withOutput(0));
     extendMotor.setControl(duty.withOutput(0));
+  }
+
+  public Command extendIntake() {
+    return Commands.run(() -> extend(), this).until(() -> atTarget(extendedPosition));
+  }
+
+  public Command retractIntake() {
+    return Commands.run(() -> retract(), this).until(() -> atTarget(retractedPosition));
+  }
+
+  public boolean atTarget(double position) {
+    // System.out.print("EJoifSFHEOIFHEOGHEGJEJGEIGJ");
+
+    double current = extendMotor.getPosition().getValueAsDouble();
+    double error = Math.abs(position - current);
+    System.out.print(error);
+    return error < 0.1;
+  }
+
+  @Override
+  public void periodic() {
+    SmartDashboard.putNumber("Intake Position", extendMotor.getPosition().getValueAsDouble());
   }
 }
