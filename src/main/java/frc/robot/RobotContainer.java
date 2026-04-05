c473c92452346cdf8d0db287bbae1d6bee84cf4a
davidhuang68 <david.huang@team7520.com>
2026-04-05 13:19:29 -0400

    Revert "doesn't work, auto changes"
    This reverts commit b05603bcd9b7f395998a1cdce5360400cb50520a.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -147,32 +147,28 @@ public class RobotContainer {
     //     "Drive SysId (Dynamic Reverse)", drive.sysIdDynamic(SysIdRoutine.Direction.kReverse));
     // autoNames.put(drive.getAutonomousCommand("Basic"), "outpost");
 
-    // autoChooser.addOption("mid auto", drive.getAutonomousCommand("middle"));
-    // // autoNames.put(drive.getAutonomousCommand("middle"), "mid auto");
+    autoChooser.addOption("mid auto", drive.getAutonomousCommand("middle"));
+    // autoNames.put(drive.getAutonomousCommand("middle"), "mid auto");
 
-    // autoChooser.addOption(
-    //     "outpost single swipe + climb", drive.getAutonomousCommand("trench to outpost auto"));
-    // // autoNames.put(drive.getAutonomousCommand("trench to outpost auto"), "central auto");
+    autoChooser.addOption(
+        "outpost single swipe + climb", drive.getAutonomousCommand("trench to outpost auto"));
+    // autoNames.put(drive.getAutonomousCommand("trench to outpost auto"), "central auto");
 
-    // autoChooser.addOption(
-    //     "depot double swipe + depot", drive.getAutonomousCommand("depot side trench auto"));
-    // autoChooser.addOption(
-    //     "outpost single swipe + outpost climbless",
-    //     drive.getAutonomousCommand("climbless trench to outpost auto"));
+    autoChooser.addOption(
+        "depot double swipe + depot", drive.getAutonomousCommand("depot side trench auto"));
+    autoChooser.addOption(
+        "outpost single swipe + outpost climbless",
+        drive.getAutonomousCommand("climbless trench to outpost auto"));
 
-    // autoChooser.addOption(
-    //     "outpost double swipe + outpost climbless",
-    //     drive.getAutonomousCommand("climbless outpost double swipe"));
+    autoChooser.addOption(
+        "outpost double swipe + outpost climbless",
+        drive.getAutonomousCommand("climbless outpost double swipe"));
 
-    // autoChooser.addOption(
-    //     "outpost double swipe + climb", drive.getAutonomousCommand("climb outpost double
-    // swipe"));
+    autoChooser.addOption(
+        "outpost double swipe + climb", drive.getAutonomousCommand("climb outpost double swipe"));
 
-    // autoChooser.addOption(
-    //     "outpost with bump", drive.getAutonomousCommand("bump climb outpost double swipe"));
-
-    // autoChooser.addOption(
-    //     "bump depot", drive.getAutonomousCommand("bump climb depot double swipe"));
+    autoChooser.addOption(
+        "outpost with bump", drive.getAutonomousCommand("bump climb outpost double swipe"));
 
     // Configure the button bindings
     configureButtonBindings();
